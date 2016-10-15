3f2ae2e9b03d7a8da563f6decf4f6822c4561c6f
Clement05 <clement.girard4@gmail.com>
2016-10-15 14:57:26 +0200

    Add connection with database

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ var fs = require('fs');
 
 var md5 = require('MD5');
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: '',
+	user: '',
+	password: '',
+	database: ''
+});
+
+connection.connect(function(err){
+	if(err){
+		console.error(err);
+	}else{
+		console.log("Connected to MySQL");
+	}
+});
 // Chargement du fichier index.html affiché au client
 
 var httpServer = http.createServer(function(req, res) {
@@ -43,42 +59,66 @@ io.sockets.on('connection', function (socket) {
     //gestion des messages
     socket.on('newmsg', function(message){
     	console.log(message);
+    
+	connection.query('SELECT * FROM users WHERE email = ?', [me.email], function(err, rows, fields){
 
-    	message.user = me;
-    	date = new Date();
-    	message.h = date.getHours();
-    	message.m = date.getMinutes();
-    	io.sockets.emit('newmsg', message);
-    	console.log(message);
-    })
+		
+		if(rows.length === 1 && rows[0].pwd === me.password){
+    			message.user = me;
+    			date = new Date();
+    			message.h = date.getHours();
+    			message.m = date.getMinutes();
+    			io.sockets.emit('newmsg', message);
+    			console.log(message);
+		}else{
+			socket.emit('error', err.code);
+		}
+	});    
+})
 
 
 
     //gestion des log
     socket.on('login', function(user){
-    	console.log(user);
-    	me = user;
-    	me.id = user.email.replace('@', '-').replace('.','-');
-    	me.avatar = 'https://gravatar.com/avatar/' + md5(user.email) + '?s=50';
-
-	var addUser = true;
-    	for(var k in users){
-    		console.log("list of user "+k);
-		console.log("id of new user "+me.id);
-		if (k ==  me.id){
-			console.log("the user is already connected");
-			addUser = false;
+	connection.query('SELECT * FROM users WHERE email = ?', [user.email], function(err, rows, fields){
+		if(err){
+			socket.emit('error', err.code);
+			return false;
 		}
-    	}
-	if(addUser === true){
-		users[me.id] = me;
+		if(rows.length === 1 && rows[0].pwd === user.password){
 
-    		io.sockets.emit('newuser', me);
-	}
-    	
+		    	console.log(user);
+    			me = user;
+    			me.email = user.email;
+			me.id = user.email.replace('@', '-').replace('.','-');
+    			me.avatar = 'https://gravatar.com/avatar/' + md5(user.email) + '?s=50';
 
-    	//socket.broadcast.emit('newuser', me);
-    	//io.sockets.emit('newuser', me);
+			var addUser = true;
+    			for(var k in users){
+    				console.log("list of user "+k);
+				console.log("id of new user "+me.id);
+				if (k ==  me.id){
+					console.log("the user is already connected");
+					addUser = false;
+				}
+    			}
+			if(addUser === true){
+				users[me.id] = me;
+				socket.emit('me',me);
+    				io.sockets.emit('newuser', me);
+			}	
+
+    			//socket.broadcast.emit('newuser', me);
+    			//io.sockets.emit('newuser', me);
+		}else{
+			var insertUser = {nickname: user.email, email: user.email, pwd: user.password};
+			console.log(insertUser);
+			var query = connection.query('INSERT INTO users SET ?', insertUser, function(err, rows, fields){
+				//console.log(err.code);
+			});
+			//console.log(query.sql);
+		}
+	});
     });
 
     socket.on('disconnect', function(){
